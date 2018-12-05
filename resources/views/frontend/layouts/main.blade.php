975c3994d3812f78433e35f6bbfdfc1993df433e
T3H <dathalongbay@gmail.com>
2018-12-05 20:37:58 +0700

    init

---
+++ resources/views/frontend/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>App Name - @yield('title')</title>
+</head>
+<body>
+@include('frontend.layouts.header')
+
+@section('sidebar')
+    đây là nội dung sidebar của layout main
+@show
+
+<div class="container">
+    @yield('content')
+</div>
+
+@include('frontend.layouts.footer')
+</body>
+</html>
