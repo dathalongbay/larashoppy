2894e51bd1d3420212165cf0a70c9b57cfa6408b
T3H <dathalongbay@gmail.com>
2018-12-05 19:36:50 +0700

    init

---
+++ app/Http/Controllers/Backend/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Index extends Controller
+{
+    //
+
+    public function index() {
+        $data = array();
+        return view('backend.index', $data);
+    }
+}
---
+++ app/Http/Controllers/Frontend/Index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Index extends Controller
+{
+    //
+    public function index() {
+        $data = array();
+        return view('frontend.index', $data);
+    }
+}
